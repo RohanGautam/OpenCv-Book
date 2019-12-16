52f405acc9972dafecd3d672e415bfc4d98bcf5d
RohanGautam <rohan1gautam@gmail.com>
2019-12-16 22:11:45 +0530

    ch3 - loading, displaying, saving images

--- load_display_save.py
+++ load_display_save.py
@@ -3,3 +3,12 @@ import cv2
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True, help="Path to image")
 args = vars(ap.parse_args()) # this is  dictionary!
+# load the image
+image = cv2.imread(args["image"]) # returns a numpy array with image data
+print("width: {} pixels".format(image.shape[1]))
+print("height: {} pixels".format(image.shape[0]))
+print("channels: {}".format(image.shape[2])) # 3 here for R,G,B
+
+cv2.imshow("Image", image)
+cv2.waitKey(0) # pauses program execution until any key is pressed
+cv2.imwrite("newpupper.png", image)
